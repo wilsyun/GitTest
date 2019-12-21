58d1e124ec1672815b3af93b11df9347dff78896
Wilson Ho <wilsyun@gmail.com>
2019-12-21 14:51:19 +0800

    insert cat

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
         public static void main(String[] args) {
+                private int cat;
                 System.out.println("Hello World !");
 				System.out.println("Hello Java !");
 				System.out.println("Hello Tibame !");
