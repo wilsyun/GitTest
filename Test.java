1badf68529cfd0c61dd6fcb2a8042aeb5eeee5ad
Wilson Ho <wilsyun@gmail.com>
2019-12-21 14:49:11 +0800

    insert dog

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
         public static void main(String[] args) {
+                private int Dog;
                 System.out.println("Hello World !");
 				System.out.println("Hello Java !");
 				System.out.println("Hello Tibame !");
